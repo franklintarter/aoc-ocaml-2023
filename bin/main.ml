0f8226c401cd6377f4d522d7a9264b95e6076281
Franklin Tarter <franklintarter@gmail.com>
2023-12-02 22:01:26 -0800

    day2 pt2

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,9 @@
 open Aoc_2023
 
 let () =
-  print_endline (string_of_int Day_01.part_one_answer);
-  print_endline (string_of_int Day_01.part_two_answer);
+  print_endline (string_of_int Day_01.part_one_answer); (* 55172 *)
+  print_endline (string_of_int Day_01.part_two_answer); (* 54925 *)
 
-  print_endline (string_of_int Day_02.part_one_answer);
+  print_endline (string_of_int Day_02.part_one_answer); (* 2164 *)
+  print_endline (string_of_int Day_02.part_two_answer); (* 69929 *)
+
--- lib/day_02.ml
+++ lib/day_02.ml
@@ -1,22 +1,23 @@
 open Core
 
-type handful = { green: int; red: int; blue: int; };;
-let empty_hand = { green = 0; red = 0; blue = 0 };;
+type color_set = { green: int; red: int; blue: int; };;
+let empty_color_set = { green = 0; red = 0; blue = 0 };;
 
-let test_handful = "3 blue, 4 red"
-let test_game = "8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+let parse_color_string str =
+  let strs = String.strip str |> String.split ~on:' ' in
+  let (h, t) = (List.hd_exn strs, List.tl_exn strs) in
+  (int_of_string h, List.hd_exn t)
 
-let parse_color_in_handful h str =
-  let s = String.strip str |> String.split ~on:' ' in
-  match List.rev s with
-| "green"::t -> { h with green = int_of_string (List.hd_exn t) }
-| "red"::t -> { h with red = int_of_string (List.hd_exn t) }
-| "blue"::t -> { h with blue = int_of_string (List.hd_exn t) }
-| _ -> raise (Invalid_argument "")
+let color_set_of_handful h str =
+  match parse_color_string str with
+  | (n, "green") -> { h with green = n }
+  | (n, "red") -> { h with red = n }
+  | (n, "blue") -> { h with blue = n }
+  | _ -> raise (Invalid_argument "")
 
-let handful_of_string str = String.split str ~on:',' |> List.fold ~init:empty_hand ~f:parse_color_in_handful
+let color_set_of_handful_string str = String.split str ~on:',' |> List.fold ~init:empty_color_set ~f:color_set_of_handful
 
-let is_impossible_handful = function
+let is_impossible_color_set = function
   | { red; green; blue } when red > 12 || green > 13 || blue > 14 -> Some ()
   | _ -> None
 
@@ -24,7 +25,10 @@ let bool_of_option = function
   | Some _ -> true
   | None -> false
 
-let is_impossible_game g = String.split g ~on:';' |> List.find_map ~f:(fun s -> handful_of_string s |> is_impossible_handful) |> bool_of_option
+let is_impossible_game game_string =
+  String.split game_string ~on:';'
+  |> List.find_map ~f:(fun s -> color_set_of_handful_string s |> is_impossible_color_set)
+  |> bool_of_option
 
 let parse_game_id str =
   match String.split str ~on:' ' with
@@ -46,4 +50,42 @@ let rec do_part_one sum channel =
   | Some line -> do_part_one ((process_line line) + sum) channel
 
 let part_one_answer = In_channel.create "./inputs/day_02.txt" |> do_part_one 0
+
+(* Part 2 *)
+
+let set_min color_set (number, color) = match color with
+| "green" when number > color_set.green -> { color_set with green = number }
+| "red" when number > color_set.red -> { color_set with red = number }
+| "blue" when number > color_set.blue -> { color_set with blue = number }
+| _ -> color_set
+
+let parse_color_string str =
+  let strs = String.strip str |> String.split ~on:' ' in
+  let (h, t) = (List.hd_exn strs, List.tl_exn strs) in
+  (int_of_string h, List.hd_exn t)
+
+let apply_handful color_set str =
+  String.split str ~on:','
+  |> List.map ~f:String.strip
+  |> List.map ~f:parse_color_string
+  |> List.fold ~init:color_set ~f:set_min
+
+let find_min_color_set = List.fold ~init:empty_color_set ~f:apply_handful
+
+let mult_color_set s = s.green * s.red * s.blue
+
+let process_line2 line =
+  let (_, game_string) = parse_game line in
+  let handful_strings = String.split ~on:';' game_string in
+  let min_color_set = find_min_color_set handful_strings in
+  mult_color_set min_color_set
+
+let rec do_part_two sum channel =
+  match In_channel.input_line channel with
+  | None -> In_channel.close channel; sum
+  | Some line -> do_part_two ((process_line2 line) + sum) channel
+
+let part_two_answer = In_channel.create "./inputs/day_02.txt" |> do_part_two 0
+
+(* don't really like how I'm mixing styles of functions, parsing with logic, but still experimenting with diff styles and language features *)
 
